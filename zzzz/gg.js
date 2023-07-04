15369cc14c50c65a096db52857ceff0964d4c51c
soyeon1008 <kimso4638@naver.com>
2023-07-05 01:02:20 +0900

    gg

--- zzzz/gg.js
+++ zzzz/gg.js
@@ -15,3 +15,36 @@ engScore = Number(engScore);
 
 let avg = (mathScore + engScore) / 2;
 console.log(avg);
+
+// helloworld 3번찍기
+// for(초기식; 조건식; 증감식) {
+// 반복할 문장 }
+for (let i = 0; i <= 2; i++) {
+  console.log("helloWorld" + i);
+}
+
+for (let index = 0; index; index++) {}
+
+// 1부터 10까지 출력하기
+for (i = 1; i <= 10; i++) {
+  console.log(i);
+}
+
+//10000까지의 숫자중에서
+// 13의 배수이면서
+// 홀수인 숫자
+
+// for (초기식; 조건식; 증감식) {
+// 반복할문장
+// }
+
+for (i = 13; i <= 10000; i++) {
+  if (i % 13 === 0 && i % 2 === 1) {
+    console.log("연습" + i);
+  }
+}
+
+// 1부터 10까지 더하기
+for (let sum; i < 11; i++) {
+  console;
+}
