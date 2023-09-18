9bdec80c0886d60cfb64d56f23150fa0ff420ff9
soyeon1008 <kimso4638@naver.com>
2023-09-18 16:38:54 +0900

    23.09.18 Typescript_start

---
+++ typescript/test.js
@@ -0,0 +1,5 @@
+var num1 = 5;
+var str2;
+str2 = "안녕하세요";
+console.log(num1);
+console.log(str2);
---
+++ typescript/test.ts
@@ -0,0 +1,5 @@
+let num1: number = 5;
+let str2: string;
+str2 = "안녕하세요";
+console.log(num1);
+console.log(str2);
